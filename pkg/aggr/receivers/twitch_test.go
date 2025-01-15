8d8bd5740cbecc5eb87e79da63cff1fda7104c97
Arthur Aslanyan <arthur.e.aslanyan@gmail.com>
2025-01-15 06:58:49 +0500

    docs(twitch): save info to work with

--- pkg/aggr/receivers/twitch_test.go
+++ pkg/aggr/receivers/twitch_test.go
@@ -14,6 +14,31 @@ import (
 	"github.com/gempir/go-twitch-irc/v4"
 )
 
+// TODO: Seems like we have to make our own websocket connection!
+// wss://irc-ws.chat.twitch.tv/
+// PONG to their PINGs, then PING them
+//
+// Seems we have to start with sending those:
+// CAP REQ :twitch.tv/tags twitch.tv/Command
+// PASS SCHMOOPIIE
+// NICK justinfan8865
+// USER justinfan8865 8 * :justinfan8865
+//
+// Then we receive
+// :tmi.twitch.tv CAP * ACK :twitch.tv/tags twitch.tv/commandsconst
+// :tmi.twitch.tv 001 justinfan8865 :Welcome, GLHF!
+// :tmi.twitch.tv 002 justinfan8865 :Your host is tmi.twitch.tv
+// :tmi.twitch.tv 003 justinfan8865 :This server is rather new
+// :tmi.twitch.tv 004 justinfan8865 :-
+// :tmi.twitch.tv 375 justinfan8865 :-
+// :tmi.twitch.tv 372 justinfan8865 :You are in a maze of twisty passages, all alike.
+// :tmi.twitch.tv 376 justinfan8865 :>
+//
+// Then we join
+// JOIN #asahnoln
+//
+// Then listen for
+// @badge-info=;badges=broadcaster/1;client-nonce=30620c76bc7d8ba00c9fe4c4d81c5ef3;color=;display-name=Asahnoln;emotes=;first-msg=0;flags=;id=6fdb1223-9252-4ee3-b4dc-c0ac88c2b372;mod=0;returning-chatter=0;room-id=39182089;subscriber=0;tmi-sent-ts=1736906306256;turbo=0;user-id=39182089;user-type= :asahnoln!asahnoln@asahnoln.tmi.twitch.tv PRIVMSG #asahnoln :Hey there!
 func TestTwitch(t *testing.T) {
 	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("req %v", r.URL)
